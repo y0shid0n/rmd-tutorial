50497e3b6bc458617dfe0f6bd51cba4c965084b9
NaohiroYoshida <y0shid0n1031@gmail.com>
2021-07-29 23:39:03 +0900

    purrrを使った並列処理

--- render.R
+++ render.R
@@ -1,4 +1,5 @@
 library(rmarkdown)
+library(purrr)
 
 df_params = read.csv("./params.csv")
 
@@ -14,3 +15,19 @@ for (i in 1:nrow(df_params)) {
          , params = list(arg01=arg01, arg02=arg02, arg03=arg03)
          )
 }
+
+# パラメータのリストから各要素を抜き出してrenderに渡す
+myfunc <- function(l) {
+  arg01 <- l[["hoge"]]
+  arg02 <- l[["fuga"]]
+  arg03 <- l[["piyo"]]
+
+  render("./sample.Rmd"
+         , output_format = "html_document"
+         , output_file = paste0("./sample_", arg01, ".html")
+         , params = list(arg01=arg01, arg02=arg02, arg03=arg03)
+  )
+}
+
+# transpose(data.frame)はlist of rowsを返す
+map(transpose(df_params), myfunc)
