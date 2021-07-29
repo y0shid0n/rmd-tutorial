5247a9f0fc5c9d046bbecb1d12b144b6fc78465a
NaohiroYoshida <y0shid0n1031@gmail.com>
2021-07-29 23:12:56 +0900

    csvからパラメータを読み込んでループ

--- render.R
+++ render.R
@@ -1,7 +1,16 @@
 library(rmarkdown)
 
-render("./sample.Rmd"
-       , output_format = "html_document"
-       , output_file = "./sample.html"
-       , params = list(arg01="hoge", arg02=999, arg03="fuga")
-       )
+df_params = read.csv("./params.csv")
+
+for (i in 1:nrow(df_params)) {
+  row <- df_params[i,]
+  arg01 <- row[["hoge"]]
+  arg02 <- row[["fuga"]]
+  arg03 <- row[["piyo"]]
+
+  render("./sample.Rmd"
+         , output_format = "html_document"
+         , output_file = paste0("./sample_", arg01, ".html")
+         , params = list(arg01=arg01, arg02=arg02, arg03=arg03)
+         )
+}
